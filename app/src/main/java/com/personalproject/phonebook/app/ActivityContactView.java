a678bb05b2afe08d4d90eb3473895be43c59f913
Asheesh Kumar <asheeshkumar75497@gmail.com>
2024-01-22 00:36:24 +0530

    contactview design.

--- app/src/main/java/com/personalproject/phonebook/app/ActivityContactView.java
+++ app/src/main/java/com/personalproject/phonebook/app/ActivityContactView.java
@@ -2,7 +2,10 @@ package com.personalproject.phonebook.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.View;
 
 import com.personalproject.phonebook.R;
 
@@ -12,5 +15,18 @@ public class ActivityContactView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_view);
+
+    }
+    public void gmailButton(View view){
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.google.android.gm");
+        if(intent != null){
+            startActivity(intent);
+        }
+    }
+    public void smsButton(View view){
+        Intent intent = getPackageManager().getLaunchIntentForPackage(Telephony.Sms.getDefaultSmsPackage(this));
+        if(intent != null){
+            startActivity(intent);
+        }
     }
 }
