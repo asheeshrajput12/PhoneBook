64b97c747b4017ef81a1738527d1ad650cdd6317
Asheesh Kumar <asheeshkumar75497@gmail.com>
2024-01-21 12:13:03 +0530

    .

--- app/src/main/java/com/personalproject/phonebook/model/CommonVar.java
+++ app/src/main/java/com/personalproject/phonebook/model/CommonVar.java
@@ -4,6 +4,7 @@ public class CommonVar {
     public static final String STR_NAME_PERSON="STR_NAME_PERSON";
     public static final String STR_NUMBER_PERSON="STR_NUMBER_PERSON";
     public static final int REQUEST_CALL_PHONE_PERMISSION = 1;
+    public static final String REQUEST_SEND_PHONE_PERMISSION ="";
 
 
 }
